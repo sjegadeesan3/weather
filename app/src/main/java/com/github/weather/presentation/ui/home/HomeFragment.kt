12346b98eb8c91f422654978b641025d504bbb8f
JEGADEESAN <sjegadeesan3@gmail.com>
2021-04-04 18:13:21 +0800

    Setup databinding

--- app/src/main/java/com/github/weather/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/github/weather/presentation/ui/home/HomeFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
 import com.github.weather.R
+import com.github.weather.databinding.HomeFragmentBinding
 import com.github.weather.domain.usecase.current.GetCurrentWeatherLatLongUseCase
 import com.github.weather.domain.usecase.forecast.GetForecastWeatherLatLongUseCase
 import kotlinx.coroutines.Dispatchers
@@ -17,12 +19,16 @@ import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: HomeFragmentBinding
     private val viewModel: HomeViewModel by sharedViewModel()
     private val getCurrentWeatherLatLongUseCase: GetCurrentWeatherLatLongUseCase by inject()
     private val getForecastWeatherLatLongUseCase: GetForecastWeatherLatLongUseCase by inject()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.home_fragment, container, false)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -30,7 +36,8 @@ class HomeFragment : Fragment() {
         viewModel.latitudeAndLongitude.observe(requireActivity(), { coordinates ->
             lifecycleScope.launch(Dispatchers.IO) {
                 val currentWeatherDetail = getCurrentWeatherLatLongUseCase.execute(coordinates)
-                Log.d("TestCurrent", "${currentWeatherDetail?.main?.temp}")
+                Log.d("TestCurrent", "${currentWeatherDetail?.sys?.country}")
+                viewModel.setCurrentWeatherDetail(currentWeatherDetail)
             }
 
             lifecycleScope.launch(Dispatchers.IO) {
--- app/src/main/java/com/github/weather/presentation/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/github/weather/presentation/ui/home/HomeViewModel.kt
@@ -3,6 +3,7 @@ package com.github.weather.presentation.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.weather.data.model.CurrentWeatherDetail
 
 class HomeViewModel : ViewModel() {
 
@@ -11,9 +12,22 @@ class HomeViewModel : ViewModel() {
         get() = mLatitudeAndLongitude
 
 
+    private val mCurrentWeatherDetail: MutableLiveData<CurrentWeatherDetail?> = MutableLiveData()
+    val currentWeatherDetail: LiveData<CurrentWeatherDetail?>
+        get() = mCurrentWeatherDetail
+
+
+    var aa: CurrentWeatherDetail? = null
+
+
     fun setLatitudeAndLongitude(latitude: String, longitude: String) {
         val latLong = Pair(latitude, longitude)
         mLatitudeAndLongitude.postValue(latLong)
+    }
+
+    fun setCurrentWeatherDetail(currentWeatherDetail: CurrentWeatherDetail?) {
+        this.mCurrentWeatherDetail.postValue(currentWeatherDetail)
+        aa = currentWeatherDetail
     }
 
 }
