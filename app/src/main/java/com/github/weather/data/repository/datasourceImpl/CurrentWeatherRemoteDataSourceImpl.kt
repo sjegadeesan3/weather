b2a2d3e734bf3fddafac11dbd39be75b7f8012d0
JEGADEESAN <sjegadeesan3@gmail.com>
2021-04-04 15:49:19 +0800

    Implementation of remote data source

--- app/src/main/java/com/github/weather/data/repository/datasourceImpl/CurrentWeatherRemoteDataSourceImpl.kt
+++ app/src/main/java/com/github/weather/data/repository/datasourceImpl/CurrentWeatherRemoteDataSourceImpl.kt
@@ -1,12 +1,13 @@
 package com.github.weather.data.repository.datasourceImpl
 
+import com.github.weather.data.api.CurrentWeatherService
 import com.github.weather.data.model.CurrentWeatherDetail
 import com.github.weather.data.repository.datasource.CurrentWeatherRemoteDataSource
 
-class CurrentWeatherRemoteDataSourceImpl : CurrentWeatherRemoteDataSource {
+class CurrentWeatherRemoteDataSourceImpl(private val currentWeatherService: CurrentWeatherService) : CurrentWeatherRemoteDataSource {
 
     override suspend fun getCurrentWeatherWithLatLong(coordinates: Pair<String, String>): CurrentWeatherDetail? {
-        TODO("Not yet implemented")
+        return currentWeatherService.getWeatherByLatLong(coordinates.first, coordinates.second).body()
     }
 
 }
--- app/src/main/java/com/github/weather/data/repository/datasourceImpl/ForecastWeatherRemoteDataSourceImpl.kt
+++ app/src/main/java/com/github/weather/data/repository/datasourceImpl/ForecastWeatherRemoteDataSourceImpl.kt
@@ -1,12 +1,13 @@
 package com.github.weather.data.repository.datasourceImpl
 
+import com.github.weather.data.api.ForecastWeatherService
 import com.github.weather.data.model.ForecastWeatherDetail
 import com.github.weather.data.repository.datasource.ForecastWeatherRemoteDataSource
 
-class ForecastWeatherRemoteDataSourceImpl : ForecastWeatherRemoteDataSource {
+class ForecastWeatherRemoteDataSourceImpl(private val forecastWeatherService: ForecastWeatherService) : ForecastWeatherRemoteDataSource {
 
     override suspend fun getCurrentWeatherWithLatLong(coordinates: Pair<String, String>): ForecastWeatherDetail? {
-        TODO("Not yet implemented")
+        return forecastWeatherService.getForecastWeatherByLatLong(coordinates.first, coordinates.second).body()
     }
 
 }
