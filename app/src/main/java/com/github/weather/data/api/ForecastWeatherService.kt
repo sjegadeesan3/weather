5de4b99c808888f7d9fefbd8dd0f9ccddd99b540
JEGADEESAN <sjegadeesan3@gmail.com>
2021-04-04 15:41:40 +0800

    Added forecast weather retrofit api interface

---
+++ app/src/main/java/com/github/weather/data/api/ForecastWeatherService.kt
@@ -0,0 +1,16 @@
+package com.github.weather.data.api
+
+import com.github.weather.BuildConfig
+import com.github.weather.data.model.ForecastWeatherDetail
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ForecastWeatherService {
+
+    @GET("forecast")
+    suspend fun getForecastWeatherByLatLong(@Query("lat")latitude: String,
+                                    @Query("lon")longitude: String,
+                                    @Query("appid")apiKey: String = BuildConfig.API_KEY): Response<ForecastWeatherDetail>
+
+}
